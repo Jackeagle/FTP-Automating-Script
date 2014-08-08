fcd98331ff9af2547e92f0256d56f31f8c6a2a92
Jackeagle <jackeagle102@gmail.com>
2014-08-08 23:21:10 +0530

    Update FTP.sh

--- FTP.sh
+++ FTP.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 cd path to directory
-HOST='HostLink'
-USER='Your_User_name'
-PASS='Your Pass'
+HOST='HostLink'                               #This is the FTP servers host or IP address.
+USER='Your_User_name'                         #This is the FTP user that has access to the server.
+PASS='Your Pass'                              #This is the password for the FTP user.
 
 ftp -nv $HOST << EOT
 user $USER $PASS
